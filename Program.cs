bdb21103aac2e7e7842bc7c13ac8ddabef5f605d
adityaoberai <adityaoberai1@gmail.com>
2022-01-10 18:55:47 +0530

    Create project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace LearnTwilio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            var accountSid = Secrets.AccountSID;
+            var authToken = Secrets.AuthToken;
+            var toNumber = Secrets.ToNumber;
+            var fromNumber = Secrets.FromNumber;
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                to: new PhoneNumber(toNumber),
+                from: new PhoneNumber(fromNumber),
+                body: "Hi there! Thanks for joining LHD Build <3"
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
